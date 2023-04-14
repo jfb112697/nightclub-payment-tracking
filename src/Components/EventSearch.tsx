125ef852dd391f680dd63cae93c5ee1f37e9eca3
Jeremy Blair <jfb112697@gmail.com>
2023-04-14 16:49:32 -0400

    Added firebase & firestore configs, when an event is selected a search is done for a collection with an id of the selected events id. if a collection isn't found it's created with a placeholder document and updates to the collection are subscribed to.

--- src/Components/EventSearch.tsx
+++ src/Components/EventSearch.tsx
@@ -1,7 +1,19 @@
 import { Button, Paper, TextField } from "@mui/material";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useLazyQuery } from "@apollo/client";
 import { TOURNAMENT_QUERY } from "../Queries/TournamentQuery";
+import {
+  CollectionReference,
+  DocumentData,
+  addDoc,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  setDoc,
+} from "firebase/firestore";
+import { useCollection } from "react-firebase-hooks/firestore";
+import db from "../firebase";
 
 type Props = {};
 
@@ -10,39 +22,79 @@ export default function EventSearch({}: Props) {
   const [getTournament, { data, loading, error }] =
     useLazyQuery(TOURNAMENT_QUERY);
 
+  const [selectedEventId, setSelectedEventId] = useState<string | null>(null);
+  const [eventCollectionRef, setEventCollectionRef] =
+    useState<CollectionReference<DocumentData> | null>(null);
+
+  const [snapshot, loadingDocuments, errorDocuments] =
+    useCollection(eventCollectionRef);
+
+  useEffect(() => {
+    if (selectedEventId) {
+      console.log(`Fetching documents for collection "${selectedEventId}"`);
+      setEventCollectionRef(collection(db, selectedEventId.toString()));
+    } else {
+      setEventCollectionRef(null);
+    }
+  }, [selectedEventId]);
+
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     getTournament({ variables: { slug: ggSlug } });
   };
 
+  async function checkAndCreateCollection(eventId: string) {
+    const eventCollectionRef = collection(db, eventId.toString());
+    const eventCollectionSnapshot = await getDocs(eventCollectionRef);
+
+    if (eventCollectionSnapshot.empty) {
+      await addDoc(eventCollectionRef, { placeholder: true });
+      console.log(`Collection with name "${eventId}" created.`);
+    } else {
+      console.log(`Collection with name "${eventId}" already exists.`);
+    }
+    setSelectedEventId(eventId);
+  }
+
   return (
     <Paper className="bg-slate-100 flex flex-col flex-[.5] p-8 justify-center gap-4">
-      <h2>Enter the slug of the tournament</h2>
-      <form className="flex flex-col gap-4 " onSubmit={handleSubmit}>
-        <TextField
-          label="Tournament Slug"
-          variant="filled"
-          value={ggSlug}
-          onChange={(c) => setGgSlug(c.target.value)}
-        ></TextField>
-        <Button variant="contained" type="submit">
-          Get Events
-        </Button>
-      </form>
-      {loading && <p>Loading...</p>}
-      {error && <p>Error: {error.message}</p>}
-      {data && (
+      {!eventCollectionRef && (
         <div>
-          <h2>{data.tournament.name}</h2>
-          <ul className="flex gap-3">
-            {data.tournament.events.map(
-              (event: { id: string; name: string }) => (
-                <Button variant="outlined" key={event.id}>
-                  {event.name}
-                </Button>
-              )
-            )}
-          </ul>
+          <h2>Enter the slug of the tournament</h2>
+          <form className="flex flex-col gap-4 " onSubmit={handleSubmit}>
+            <TextField
+              label="Tournament Slug"
+              variant="filled"
+              value={ggSlug}
+              onChange={(c) => setGgSlug(c.target.value)}
+            ></TextField>
+            <Button variant="contained" type="submit">
+              Get Events
+            </Button>
+          </form>
+          {loading && <p>Loading...</p>}
+          {error && <p>Error: {error.message}</p>}
+          {data && (
+            <div>
+              <h2>{data.tournament.name}</h2>
+              <ul className="flex gap-3">
+                {data.tournament.events.map(
+                  (event: { id: string; name: string }) => {
+                    console.log(event.id);
+                    return (
+                      <Button
+                        onClick={() => checkAndCreateCollection(event.id)}
+                        variant="outlined"
+                        key={event.id}
+                      >
+                        {event.name}
+                      </Button>
+                    );
+                  }
+                )}
+              </ul>
+            </div>
+          )}
         </div>
       )}
     </Paper>
---
+++ src/firebase.tsx
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { getFirestore } from "@firebase/firestore";
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDYFQtDExbn-h51_VcwsWWiLUGVqVCDpxM",
+  authDomain: "nightclub-payment-tracking.firebaseapp.com",
+  projectId: "nightclub-payment-tracking",
+  storageBucket: "nightclub-payment-tracking.appspot.com",
+  messagingSenderId: "522610216554",
+  appId: "1:522610216554:web:1f53c4e010fc5da1890413",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default db;
